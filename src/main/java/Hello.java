1d0e1ae38c7ab883d2ab3b0e7f8fc07547e5f446
Alexey_Korotayev <Alexey_Korotayev@epam.com>
2019-01-28 15:15:15 +0600

    Initial commit

---
+++ src/main/java/Hello.java
@@ -0,0 +1,10 @@
+public class Hello {
+
+    public  static void main (String[] args){
+        System.out.printf(hello());
+    }
+    public  static String hello()
+    {
+        return "Hello !!!";
+    }
+}
